92d1d6c119d8d3ceab6d181ad2bd63ae94deb828
Jack Carroll <jackcarroll9000@gmail.com>
2019-12-19 12:29:57 +0000

    Testing a new technology for Games Middleware with a simple tutorial for getting the user's geolocation using an IP address

---
+++ Geolocation Emerging Tech Test/Geolocation Emerging Tech Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Geolocation_Emerging_Tech_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
