8fa92f446fa947caf4c17559cad11a4b3c3ab6e2
Jack Carroll <jackcarroll9000@gmail.com>
2019-12-19 16:36:49 +0000

    NuGet package installed for IP Address to determine location for weather tracker for that specific area. Geolocation put into use here

--- Geolocation Emerging Tech Test/Geolocation Emerging Tech Test/Controllers/HomeController.cs
+++ Geolocation Emerging Tech Test/Geolocation Emerging Tech Test/Controllers/HomeController.cs
@@ -5,25 +5,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Geolocation_Emerging_Tech_Test.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using FreeGeoIPCore;
+using MetOfficeDataPoint;
+using MetOfficeDataPoint.Models;
+using MetOfficeDataPoint.Models.GeoCoordinate;
+
 
 namespace Geolocation_Emerging_Tech_Test.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpContextAccessor _httpConAccessor;
+        private readonly IConfiguration _con;
+
+
+
+        public HomeController(IConfiguration conf, IHttpContextAccessor httpConAccessor)
         {
-            return View();
+            _con = conf;
+            _httpConAccessor = httpConAccessor;
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Index(double lon, double lat)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            FreeGeoIPClient clientIP = new FreeGeoIPClient();      
+
+             string ipAdd = WeatherGeolocation.GetIPRequest(_httpConAccessor);
+
+            FreeGeoIPCore.Models.Location location = clientIP.GetLocation(ipAdd).Result;
+
+            GeoCoordinate geoCoordinate = new GeoCoordinate();
+
+            if(lon == 0 && lat == 0)
+            {
+                geoCoordinate.Longitude = location.Longitude;
+                geoCoordinate.Latitude = location.Latitude;
+            }
+            else
+            {
+                geoCoordinate.Longitude = lon;
+                geoCoordinate.Latitude = lat;
+            }
+            return View();
+        }     
     }
 }
---
+++ Geolocation Emerging Tech Test/Geolocation Emerging Tech Test/Models/WeatherGeolocation.cs
@@ -0,0 +1,56 @@
+﻿using FreeGeoIPCore.AppCode;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geolocation_Emerging_Tech_Test.Models
+{
+    public class WeatherGeolocation
+    {
+        public static string GetIPRequest(IHttpContextAccessor httpConAccessor, bool attemptUsingXForwardHeader = true)
+        {
+            string ipAddress = null;
+
+            if(attemptUsingXForwardHeader)
+            {
+                ipAddress = GetHeaderValAs<string>(httpConAccessor, "X-Forwarded-For").SplitCsv().FirstOrDefault();
+            }
+
+            if (ipAddress.IsNullOrWhitespace() && httpConAccessor.HttpContext?.Connection?.RemoteIpAddress != null)
+                ipAddress = httpConAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            if (ipAddress.IsNullOrWhitespace())
+                ipAddress = GetHeaderValAs<string>(httpConAccessor, "REMOTE_ADDR");
+
+            // _httpContextAccessor.HttpContext?.Request?.Host this is the local host.
+
+            if (ipAddress.IsNullOrWhitespace())
+                throw new Exception("Unable to determine caller's IP.");
+
+            // Remove port if on IP address
+            ipAddress = ipAddress.Substring(0,ipAddress.IndexOf(":"));
+
+
+            return ipAddress;
+        }
+
+
+        public static T GetHeaderValAs<T>(IHttpContextAccessor httpConAccessor, string headerName)
+        {
+            StringValues vals;
+
+            if (httpConAccessor.HttpContext?.Request?.Headers?.TryGetValue(headerName, out vals) ?? false)
+            {
+                string rawValues = vals.ToString();   // writes out as Csv when there are multiple.
+
+                if (!rawValues.IsNullOrWhitespace())
+                    return (T)Convert.ChangeType(vals.ToString(), typeof(T));
+            }
+            return default(T);
+        }
+
+    }
+}
