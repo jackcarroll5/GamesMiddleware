8057b2ef8d47e82157a9d1356361e1ea3c2b1d86
Jack Carroll <jackcarroll9000@gmail.com>
2019-12-19 18:52:14 +0000

    The geolocation tutorial to get the location of a particular city or country according to an ip address based on this developer's ip address to test on an emerging technology following from the complicated first tutorial

---
+++ GeolocationGamesMiddlewareEmergingTech/GeolocationGamesMiddlewareEmergingTech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeolocationGamesMiddlewareEmergingTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
