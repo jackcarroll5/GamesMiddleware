cf06a394f2d2d472dfa29901e3817cb3d7fee702
Jack Carroll <jackcarroll9000@gmail.com>
2019-12-20 21:46:07 +0000

    A geolocation mini-project in Unity for Android using the LocationService tool to get your current location using the GPS on your phone. Details include the timestamp, altitude and horizontal and vertical accuracy.

---
+++ Geolocation Unity Style/Assets/GeolocationUnityTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeolocationUnityTest : MonoBehaviour
+{
+    public string locale;
+    public Text geolocationText;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+     if (!Input.location.isEnabledByUser)
+    {
+           
+    }
+
+        Input.location.Start();
+        Debug.Log("Location status: " + Input.location.status);
+       
+        int maximumWait = 10;
+
+        while (Input.location.status
+               == LocationServiceStatus.Initializing && maximumWait > 0) {
+            yield return new WaitForSeconds (1);
+            maximumWait--;
+        }
+ 
+        if (maximumWait < 1) {
+            print ("The service is timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed) {
+            print ("Unable to determine the current device location");
+            yield break;
+        }
+        else {
+        locale = "Latitude: " + Input.location.lastData.latitude + "\nLongitude: " + Input.location.lastData.longitude + "\nAltitude: " + Input.location.lastData.altitude + "\nHorizontal Accuracy: " + Input.location.lastData.horizontalAccuracy + "\nTimestamp: " +
+          Input.location.lastData.timestamp + "\nVertical Accuracy: " + Input.location.lastData.verticalAccuracy;
+                                       
+                   geolocationText.text = "Current GPS coordinates: \n" + "Lat: " + Input.location.lastData.latitude.ToString() + " Lon: " + Input.location.lastData.longitude.ToString() + "\n" + locale;
+        }
+        Input.location.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
