a0722f8928e32396fb1a6facaca7c56f522ca92b
Jack Carroll <jackcarroll9000@gmail.com>
2019-12-20 14:02:02 +0000

    Shows current location of user using the GeoWatcher feature preinstalled into Visual Stduio that involves System.Device reference to access location. Extra details in label string to specify location, e.g. horizontal accuracy

--- GeolocationWithGeolocation.NETPackageTest/GeolocationWithGeolocation.NETPackageTest/Form1.Designer.cs
+++ GeolocationWithGeolocation.NETPackageTest/GeolocationWithGeolocation.NETPackageTest/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblLocationPosition = new System.Windows.Forms.Label();
+            this.btnLocationRetrieval = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(259, 30);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(229, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Retrieving the current location";
+            // 
+            // lblLocationPosition
+            // 
+            this.lblLocationPosition.AutoSize = true;
+            this.lblLocationPosition.Location = new System.Drawing.Point(259, 79);
+            this.lblLocationPosition.Name = "lblLocationPosition";
+            this.lblLocationPosition.Size = new System.Drawing.Size(0, 17);
+            this.lblLocationPosition.TabIndex = 1;
+            // 
+            // btnLocationRetrieval
+            // 
+            this.btnLocationRetrieval.Location = new System.Drawing.Point(320, 343);
+            this.btnLocationRetrieval.Name = "btnLocationRetrieval";
+            this.btnLocationRetrieval.Size = new System.Drawing.Size(124, 60);
+            this.btnLocationRetrieval.TabIndex = 2;
+            this.btnLocationRetrieval.Text = "Get Location";
+            this.btnLocationRetrieval.UseVisualStyleBackColor = true;
+            this.btnLocationRetrieval.Click += new System.EventHandler(this.btnLocationRetrieval_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnLocationRetrieval);
+            this.Controls.Add(this.lblLocationPosition);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Geolocation Position Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblLocationPosition;
+        private System.Windows.Forms.Button btnLocationRetrieval;
     }
 }
 
--- GeolocationWithGeolocation.NETPackageTest/GeolocationWithGeolocation.NETPackageTest/Form1.cs
+++ GeolocationWithGeolocation.NETPackageTest/GeolocationWithGeolocation.NETPackageTest/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Device.Location;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,44 @@ using System.Windows.Forms;
 
 namespace GeolocationWithGeolocation.NETPackageTest
 {
+    //Reference - Some of the actions based on tutorial followed for getting location within the Windows Form with the .NET Framework using the GeoCoordinateWatcher feature - http://irisclasson.com/2015/09/21/geolocation-net-4-and-up-requested-winforms-example/
     public partial class Form1 : Form
     {
+        private GeoCoordinateWatcher _geolocationwatcher;
+
         public Form1()
         {
             InitializeComponent();
+
+            _geolocationwatcher = new GeoCoordinateWatcher();
+
+            _geolocationwatcher.StatusChanged += GeoLocationChangedStatus;
+
         }
+
+        private void GeoLocationChangedStatus(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            if (e.Status != GeoPositionStatus.Ready)
+                return;
+
+            GeoPosition<GeoCoordinate> coordinates = _geolocationwatcher.Position;
+
+            GeoCoordinate devPosition = coordinates.Location;
+
+            DateTimeOffset retrievedAt = coordinates.Timestamp;
+
+            GeoPositionPermission allow = _geolocationwatcher.Permission;
+
+            lblLocationPosition.Text = string.Format("Latitude: {0}, Longitude: {1}, retrieved at: {2},\nVertical Accuracy : {3},\n Speed : {4},\n Altitude : {5},\n Course : {6},\n Is Location Unknown : {7},\n Horizontal Accuracy : {8}", devPosition.Latitude, devPosition.Longitude, retrievedAt.DateTime.ToLongTimeString(),devPosition.VerticalAccuracy,devPosition.Speed,devPosition.Altitude, devPosition.Course, devPosition.IsUnknown.ToString(),devPosition.HorizontalAccuracy);
+        }
+
+        private void btnLocationRetrieval_Click(object sender, EventArgs e)
+        {
+            _geolocationwatcher.Start();
+
+            btnLocationRetrieval.Enabled = false;
+        }
+
+       
     }
 }
